ef613eb80415c7d6583ae37715a99798bbb144eb
akent <alexanderkent099@gmail.com>
2024-04-14 12:49:51 -0400

    Add navigation component providing navigation links using NavLink from react-router-dom allowing users to navigate between different pages in the app

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => (
+    <nav>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/portfolio">Portfolio</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/resume">Resume</NavLink>
+    </nav>
+);
