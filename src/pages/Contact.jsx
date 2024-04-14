85a8b4fa8c5b888f983f7cda3eeca4605f99dad4
akent <alexanderkent099@gmail.com>
2024-04-14 13:25:14 -0400

    Adding contact component

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Contact = () => (
+    <section>
+        <h2>Contact</h2>
+        <p>This is the contact portion!</p>
+    </section>
+);
+
+export default Contact;
