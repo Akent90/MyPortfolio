f4dc9f0fd238fc349dbf811a3f603b477b5fb6f6
akent <alexanderkent099@gmail.com>
2024-04-14 13:41:55 -0400

    Create Resume component

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Resume = () => (
+    <section>
+        <h2>Resume</h2>
+        <p>This will be the section with a link to download my resume!</p>
+    </section>
+);
+
+export default Resume;
