c47819ec6e4f736b69a89301d5b42efdd8b0c7e8
akent <alexanderkent099@gmail.com>
2024-04-14 13:23:03 -0400

    Add About component, which represents the About Me section of the app

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+    <section>
+        <h2>About Me</h2>
+        <p>Here is where to add some content about me.</p>
+    </section>
+);
+
+export default About;
