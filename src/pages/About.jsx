59da8f79e83863c42dca23012aee0e5c852ce061
akent <alexanderkent099@gmail.com>
2024-04-16 12:49:57 -0400

    Adding About Me content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,35 @@ import React from 'react';
 const About = () => (
     <section>
         <h2>About Me</h2>
-        <p>Here is where to add some content about me.</p>
+        <p>
+            Hi, I'm Alex! At the core of my professional and personal ethos lies a
+            deep commitment to the principles of LDRSHIP - Loyalty, Duty, Respect,
+            Selfless Service, Honor, Integrity, and Personal Courage. These
+            values, instilled in me during my time in the Army, continue to guide
+            my approach to life and work. I am also a strong proponent of the
+            servant leader model, believing that true leadership comes from
+            serving and empowering others. Military service, coupled with a
+            frontline role in my community hospital during the Covid-19 pandemic,
+            has profoundly shaped my perspective. These challenging, yet enriching
+            experiences, have reinforced my belief in teamwork, resilience, and
+            the relentless pursuit of meaningful goals.
+        </p>
+        <p>
+            As I look to the future, I aim to be an integral part of a team, lead
+            with purpose, and contribute to building something that positively
+            impacts our collective digital future. My journey has been one of continuous learning, adaptation, and a
+            relentless pursuit of excellence, always with an eye towards serving
+            others and bettering our world.
+        </p>
+        <p>
+            I was first introduced to programming through my occupational
+            specialty in the Army. Executing complex programming protocols for
+            radar hard drives, configuring and managing local area networks, and
+            optimizing functionality and security in mission-critical situations
+            completely solidified my love for functional utility. I am driven to
+            integrate this practical reliability with streamlined efficiency to
+            continuously bridge utility and elegance.
+        </p>
     </section>
 );
 
